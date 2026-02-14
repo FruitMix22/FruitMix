79db055a12acdeb23f78466098274fb5e7600f2a
FruitMix22 <coreymedlandweal@gmail.com>
2026-02-14 15:02:52 +0000

    Base Tutorial script

---
+++ VulkanTutorialTest/VulkanTutorialTest.cpp
@@ -0,0 +1,12 @@
+﻿// VulkanTutorialTest.cpp : Defines the entry point for the application.
+//
+
+#include "VulkanTutorialTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
