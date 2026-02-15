09478225c065e58ea1866354db72f80f3160b560
FruitMix22 <coreymedlandweal@gmail.com>
2026-02-15 13:20:35 +0000

    Initialise GLFW window

--- VulkanTutorialTest/VulkanTutorialTest.cpp
+++ VulkanTutorialTest/VulkanTutorialTest.cpp
@@ -1,12 +1,82 @@
-﻿// VulkanTutorialTest.cpp : Defines the entry point for the application.
-//
+﻿#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+#include <GLFW/glfw3.h>
 
-#include "VulkanTutorialTest.h"
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-using namespace std;
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+
+    // window context
+    GLFWwindow* window;
+
+
+
+    void initWindow()
+    {
+        // ititialise glfw
+        glfwInit();
+
+        // Disable automatic OpenGL context
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // No resizing allowed (for now!)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // Create window
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan()
+    {
+
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+    void cleanup()
+    {
+        // GLFW
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    HelloTriangleApplication app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
